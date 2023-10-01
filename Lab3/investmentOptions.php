93b020a6a03188348e2cd76621b871305b681204
Krishigina <krishigina@gmial.com>
2023-10-01 16:54:58 +0300

    Простейшая программа на PHP. Конвертация статического контента в динамический

---
+++ Lab3/investmentOptions.php
@@ -0,0 +1,97 @@
+<?php
+$string =  '<!DOCTYPE html>'.
+'<html lang="ru">'.
+''.
+'<head>'.
+'    <meta charset="UTF-8">'.
+'    <title>Шишигина 221-362 Лаб3</title>'.
+'    <link rel="preconnect" href="https://fonts.googleapis.com">'.
+'    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>'.
+'    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">'.
+'    <link rel="stylesheet" href="styles/main.css">'.
+'</head>'.
+''.
+'<body>'.
+'    <header class="header">'.
+'        <div class="container">'.
+'            <nav class="main-menu">'.
+'                <a href="index.php">Описание</a>'.
+'                <a href="#investment-options">Виды инвестиций</a>'.
+'                <a href="#strategies">Стратегии</a>'.
+'            </nav>'.
+'            <div class="text-center py-5">'.
+'                <h1 class="name">Инвестиционная компания</h1>'.
+'                <h2>Управление инвестициями для финансовой стабильности</h2>'.
+'                <p class="w-50 my-3 mx-auto">'.
+'                    Мы предлагаем нашим клиентам достичь финансовой независимости через управление инвестициями'.
+'                </p>'.
+'                <a class="btn contact-me-btn" href="mailto:info@dohod.com">Свяжитесь с нами</a>'.
+'            </div>'.
+'        </div>'.
+'    </header>'.
+''.
+'    <main>'.
+'        <div class="container">'.
+''.
+'            <section id="investment-options">'.
+'                <p></p>'.
+'                <h1>Виды инвестиций</h1>'.
+'                <p>'.
+'                    Компания предлагает широкий спектр инвестиционных возможностей с различными уровнями риска и'.
+'                    доходности.'.
+'                    Наши клиенты могут выбирать из таких видов инвестиций:'.
+'                </p>'.
+'                <table>'.
+'                    <tr>'.
+'                        <th>Вид инвестиции</th>'.
+'                        <th>Описание</th>'.
+'                        <th>Уровень риска</th>'.
+'                        <th>Ожидаемая доходность</th>'.
+'                    </tr>'.
+'                    <tr>'.
+'                        <td>Акции на фондовом рынке</td>'.
+'                        <td>Инвестирование в акции компаний на фондовом рынке</td>'.
+'                        <td>Высокий</td>'.
+'                        <td>Высокая</td>'.
+'                    </tr>'.
+'                    <tr>'.
+'                        <td>Облигации и ценные бумаги</td>'.
+'                        <td>Инвестирование в облигации и другие ценные бумаги</td>'.
+'                        <td>Средний</td>'.
+'                        <td>Средняя</td>'.
+'                    </tr>'.
+'                    <tr>'.
+'                        <td>Инвестиции в недвижимость</td>'.
+'                        <td>Покупка и аренда недвижимости для получения дохода</td>'.
+'                        <td>Низкий</td>'.
+'                        <td>Стабильная</td>'.
+'                    </tr>'.
+'                    <tr>'.
+'                        <td>Финансирование стартапов и инновационных проектов</td>'.
+'                        <td>Инвестирование в молодые компании с высоким потенциалом</td>'.
+'                        <td>Высокий</td>'.
+'                        <td>Высокая</td>'.
+'                    </tr>'.
+'                </table>'.
+''.
+'                <div class="text-right">'.
+'                    <p>'.
+''.
+'                    </p>'.
+'                </div>'.
+'            </section>'.
+'        </div>'.
+'    </main>'.
+''.
+'    <footer class="footer">'.
+'        <div class="container">'.
+'            &copy; 2023 Инвестиционная компания "Доход"'.
+'        </div>'.
+'    </footer>'.
+''.
+'</body>'.
+''.
+'</html>';
+
+ echo $string; 
+?>
---
+++ Lab3/strategies.php
@@ -0,0 +1,68 @@
+<?php
+$string =  '<!DOCTYPE html>'.
+'<html lang="ru">'.
+''.
+'<head>'.
+'    <meta charset="UTF-8">'.
+'    <title>Шишигина 221-362 Лаб3</title>'.
+'    <link rel="preconnect" href="https://fonts.googleapis.com">'.
+'    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>'.
+'    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">'.
+'    <link rel="stylesheet" href="styles/main.css">'.
+'</head>'.
+''.
+'<body>'.
+'    <header class="header">'.
+'        <div class="container">'.
+'            <nav class="main-menu">'.
+'                <a href="index.php">Описание</a>'.
+'                <a href="investmentOptions.php">Виды инвестиций</a>'.
+'                <a href="#strategies">Стратегии</a>'.
+'            </nav>'.
+'            <div class="text-center py-5">'.
+'                <h1 class="name">Инвестиционная компания</h1>'.
+'                <h2>Управление инвестициями для финансовой стабильности</h2>'.
+'                <p class="w-50 my-3 mx-auto">'.
+'                    Мы предлагаем нашим клиентам достичь финансовой независимости через управление инвестициями'.
+'                </p>'.
+'                <a class="btn contact-me-btn" href="mailto:info@dohod.com">Свяжитесь с нами</a>'.
+'            </div>'.
+'        </div>'.
+'    </header>'.
+''.
+'    <main>'.
+'        <div class="container">'.
+'            <section id="strategies">'.
+'                <p></p>'.
+'                <h1>Стратегии</h1>'.
+'                <h2 class="h2">Стратегии в инвестиционном планировании необходимы для определения целей и пути их достижения, а также управления 
+рисками и максимизации доходности. Они помогают инвесторам принимать осознанные решения, основанные на анализе рынка и прогнозировании 
+возможных сценариев, что способствует повышению эффективности инвестиций и снижению возможных потерь.</h2>'.
+'                <p>'.
+'                    Наша команда экспертов разработала ряд эффективных инвестиционных стратегий, которые позволяют'.
+'                    нашим клиентам максимизировать свой доход и управлять рисками. Наши стратегии включают:'.
+'                </p>'.
+'                <ul>'.
+'                    <li>Долгосрочные инвестиции в устойчивые компании</li>'.
+'                    <li>Активное трейдинговое управление портфелем</li>'.
+'                    <li>Диверсификация инвестиций для снижения риска</li>'.
+'                    <li>Использование алгоритмического трейдинга и искусственного интеллекта</li>'.
+'                </ul>'.
+'                <div class="text-right">'.
+'                </div>'.
+'            </section>'.
+'        </div>'.
+'    </main>'.
+''.
+'    <footer class="footer">'.
+'        <div class="container">'.
+'            &copy; 2023 Инвестиционная компания "Доход"'.
+'        </div>'.
+'    </footer>'.
+''.
+'</body>'.
+''.
+'</html>';
+
+ echo $string; 
+?>
