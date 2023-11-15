1178f5c35b6d9bc79c299929f0c4d8ab88dbfd45
Krishigina <krishigina@gmial.com>
2023-11-15 13:48:03 +0300

    Добавлен ввод переменных на сайте пользователем

--- Lab9/index.php
+++ Lab9/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Лабораторная работа</title>
+    <title>Лабораторная работа №9</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
@@ -23,78 +23,109 @@
     </header>
 
     <main class="main">
+        <form method="post" action="" class="contact-form">
+            <label class="label" for="startArgument">Начальный аргумент:</label>
+            <input type="number" name="startArgument" id="startArgument" class="form-field" required>
+            <br>
+            <label for="numValues">Количество значений функции:</label>
+            <input type="number" name="numValues" id="numValues" class="form-field" required>
+            <br>
+            <label for="step">Шаг:</label>
+            <input type="number" name="step" id="step" class="form-field" required>
+            <br>
+            <label for="layoutType">Тип верстки:</label>
+            <select name="layoutType" id="layoutType" class="form-field" >
+                <option value="A">Тип A</option>
+                <option value="B">Тип B</option>
+                <option value="C">Тип C</option>
+                <option value="D">Тип D</option>
+                <option value="E">Тип E</option>
+            </select>
+            <br>
+            <button type="submit" class="btn">Рассчитать</button>
+        </form>
+
         <?php
-        // Явная инициализация числовых переменных
-        $startArgument = 1;
-        $numValues = 10;
-        $step = 2;
-        $maxValue = 100;
-        $minValue = -100;
-        $values = []; // Перемещение объявления и инициализации массива $values
-        
-        // Явная инициализация строковой переменной для типа формируемой верстки
-        $layoutType = 'A';
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Получаем значения из формы и присваиваем их переменным
+            $startArgument = (int) $_POST["startArgument"];
+            $numValues = (int) $_POST["numValues"];
+            $step = (int) $_POST["step"];
+            $layoutType = $_POST["layoutType"];
 
-        // Вывод типа верстки в подвале
-        echo "<footer><p>Тип верстки: $layoutType</p></footer>";
+            // Вывод типа верстки в подвале
+            echo "<footer><p>Тип верстки: $layoutType</p></footer>";
 
-        // Вычисление значений функции и их вывод в соответствии с выбранным типом верстки
-        for ($i = 0; $i < $numValues; $i++) {
-            $k=$i+1;
-            $argument = $startArgument + ($i * $step);
+            $values = []; // Перемещение объявления и инициализации массива $values
         
-            // В зависимости от типа верстки, используем соответствующий код
-            if ($layoutType == 'A') {
-                $value = ($argument <= 10) ? (10 * $startArgument - 5) : (($argument > 10 && $argument < 20) ? (($argument + 3) * pow($argument, 2)) : (3 / ($startArgument - 25)));
-                $values[] = $value; 
-                echo "f($argument) = $value<br><br>";
-            } elseif ($layoutType == 'B') {
-                $value = ($argument <= 10) ? (10 * $startArgument - 5) : (($argument > 10 && $argument < 20) ? (($argument + 3) * pow($argument, 2)) : (3 / ($startArgument - 25)));
-                $values[] = $value; 
-                echo "<ul><li>f($argument) = $value</li></ul>";
-            } elseif ($layoutType == 'C') {
-                $value = ($argument <= 10) ? (10 * $startArgument - 5) : (($argument > 10 && $argument < 20) ? (($argument + 3) * pow($argument, 2)) : (3 / ($startArgument - 25)));
-                $values[] = $value;
-                echo "<ol start='$k'><li>f($argument) = $value</li></ol>";
-            } elseif ($layoutType == 'D') {
-                $value = ($argument <= 10) ? (10 * $startArgument - 5) : (($argument > 10 && $argument < 20) ? (($argument + 3) * pow($argument, 2)) : (3 / ($startArgument - 25)));
-                $values[] = $value; 
-                echo "<table border='1' cellspacing='0' cellpadding='10'>
-                    <tr>
-                        <td>#</td>
-                        <td>Аргумент</td>
-                        <td>Значение функции</td>
-                    </tr>
-                    <tr>
-                        <td>$i</td>
-                        <td>$argument</td>
-                        <td>$value</td>
-                    </tr>
-                </table>";
-            } elseif ($layoutType == 'E') {
-                $value = ($argument <= 10) ? (10 * $startArgument - 5) : (($argument > 10 && $argument < 20) ? (($argument + 3) * pow($argument, 2)) : (3 / ($startArgument - 25)));
-                $values[] = $value; // Добавить эту строку
-                echo "<div class='typeE'>
-                    f($argument) = $value
-                </div>";
+            // Вычисление значений функции и их вывод в соответствии с выбранным типом верстки
+            for ($i = 0; $i < $numValues; $i++) {
+                $k = $i + 1;
+                $argument = $startArgument + ($i * $step);
+            
+                // В зависимости от типа верстки, используем соответствующий код
+                if ($layoutType == 'A' || $layoutType == 'B' || $layoutType == 'C' || $layoutType == 'D') {
+                    if ($startArgument == 25) {
+                        $value = 'Ошибка';
+                    } else {
+                        $value = ($argument <= 10) ? (10 * $startArgument - 5) : (($argument > 10 && $argument < 20) ? (($argument + 3) * pow($argument, 2)) : (3 / ($startArgument - 25)));
+                    }
+                    $values[] = $value;
+            
+                    if ($layoutType == 'A') {
+                        echo "f($argument) = $value<br><br>";
+                    } elseif ($layoutType == 'B') {
+                        echo "<ul><li>f($argument) = $value</li></ul>";
+                    } elseif ($layoutType == 'C') {
+                        echo "<ol start='$k'><li>f($argument) = $value</li></ol>";
+                    } elseif ($layoutType == 'D') {
+                        echo "<table border='1' cellspacing='0' cellpadding='10'>
+                            <tr>
+                                <td>#</td>
+                                <td>Аргумент</td>
+                                <td>Значение функции</td>
+                            </tr>
+                            <tr>
+                                <td>$i</td>
+                                <td>$argument</td>
+                                <td>$value</td>
+                            </tr>
+                        </table>";
+                    }
+                } elseif ($layoutType == 'E') {
+                    if ($startArgument == 25) {
+                        $value = 'Ошибка';
+                    } else {
+                        $value = ($argument <= 10) ? (10 * $startArgument - 5) : (($argument > 10 && $argument < 20) ? (($argument + 3) * pow($argument, 2)) : (3 / ($startArgument - 25)));
+                    }
+                    $values[] = $value;
+            
+                    echo "<div class='typeE'>
+                        f($argument) = $value
+                    </div>";
+                }
             }
+
+
+            // Вычисление и вывод максимального, минимального, среднего арифметического и суммы значений функции
+            $max = max($values);
+            $min = min($values);
+            $average = array_sum($values) / count($values);
+            $sum = array_sum($values);
+
+            echo "<br>Максимальное значение: $max<br><br>";
+            echo "Минимальное значение: $min<br><br>";
+            echo "Среднее арифметическое: $average<br><br>";
+            echo "Сумма значений: $sum<br>";
         }
-
-        // Вычисление и вывод максимального, минимального, среднего арифметического и суммы значений функции
-        $max = max($values);
-        $min = min($values);
-        $average = array_sum($values) / count($values);
-        $sum = array_sum($values);
-
-        echo "<br>Максимальное значение: $max<br><br>";
-        echo "Минимальное значение: $min<br><br>";
-        echo "Среднее арифметическое: $average<br><br>";
-        echo "Сумма значений: $sum<br>";
         ?>
     </main>
 
     <footer class="footer">
         <div class="container">
+            <?php
+            echo "<footer><p>Тип верстки: $layoutType</p></footer>";
+            ?>
             &copy; 2023 Циклические алгоритмы. Условия в алгоритмах. Табулирование функций
         </div>
     </footer>
